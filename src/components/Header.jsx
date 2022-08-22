9cfc44b8b74f193fa0f721ec86b3351b11a873f9
Mateo-olier <aolierguerrero@outlook.com>
2022-08-22 07:59:32 -0500

    corrigiendo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,27 +5,27 @@ export default function Header() {
     <div>
       <header className="inline-flex w-full mb-10">
         <div className="flex items-center">
-          <img src="./img/logo.png" className="m-2" />
+          <img src="./img/logo.png" className="m-2" alt="logo buho"/>
           <h4 className="text-lg uppercase">dev olier</h4>
         </div>
         <ul className="flex ml-auto items-center space-x-6 uppercase">
           <li>
-            <a href="#" className="border-2 border-black p-2">
+            <a href="/" className="border-2 border-black p-2">
               Inicio
             </a>
           </li>
           <li>
-            <a href="#" className="text-base">
+            <a href="/" className="text-base">
               About us
             </a>
           </li>
           <li>
-            <a href="#" className="text-base">
+            <a href="/" className="text-base">
               Portafolio
             </a>
           </li>
           <li>
-            <a href="#" className="text-base">
+            <a href="/" className="text-base">
               Contact
             </a>
           </li>
